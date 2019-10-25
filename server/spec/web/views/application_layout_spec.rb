bac036594ad7be763728133f0afad3a22bf01913
Dima Boyko <dimka.boyko@gmail.com>
2019-10-25 21:45:04 +0200

    refactored structure (#10)

--- server/spec/web/views/application_layout_spec.rb
+++ server/spec/web/views/application_layout_spec.rb
@@ -1,9 +1,8 @@
 require "spec_helper"
 
 RSpec.describe Web::Views::ApplicationLayout, type: :view do
-  let(:layout)   { Web::Views::ApplicationLayout.new(template, {}) }
+  let(:layout)   { Web::Views::ApplicationLayout.new({ format: :html }, "contents") }
   let(:rendered) { layout.render }
-  let(:template) { Hanami::View::Template.new('apps/web/templates/application.html.erb') }
 
   it 'contains application name' do
     expect(rendered).to include('Web')
